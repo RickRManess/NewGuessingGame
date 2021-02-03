92efc03e0f2092921d706f4f56d1d69887f8e941
Richard Maness <rickrmaness@gmail.com>
2021-02-03 17:53:40 -0500

    Add project files.

---
+++ NewGuessingGame/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace NewGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            int guess;
+            int secret;
+            int counter;
+
+        // Introduction Text
+        start:
+            Console.Clear();
+            Console.WriteLine("Lets Play a Game....");
+            Console.WriteLine("Pick a number between 1 and 100");
+
+            //Secret Number Created
+            secret = rand.Next(100);
+            counter = 0;
+
+            //UserGuess Input
+            //Game Loop With Count Limit 10 
+            while (counter< 10)
+                {
+                  
+                guess = Int32.Parse(Console.ReadLine());
+
+                    if (secret > guess)
+                    {
+                        Console.WriteLine("Too Low");
+                    }
+                    else if (secret < guess)
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Great Job!!! You Guessed it!!");
+                    goto start;
+                    
+                    }
+                    counter++;
+                }
+            Console.WriteLine("You Lose");
+            
+                
+        }   
+    }
+}
+for (int i = 0; i <= 2; i++)
+{
+    for (int j = 0; j <= 2; j++)
+    {
+        slotMachine[i, j] = rand.Next(3);
+        Console.Write(slotMachine[i, j]);
+
+    }
